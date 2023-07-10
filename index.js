b8ab36640c8e3e9d3e114634508ee93a0fdbeb33
Patrick Bond <pbond@tradingeye.com>
2023-07-10 12:16:27 +0100

    1st Commit

---
+++ index.js
@@ -0,0 +1,17 @@
+function onimage()
+{this.setAttribute("src","img/1964417_github_logo_media_social_icon.png");
+}
+
+function outimage()
+{this.setAttribute("src","img/6214513_github_logo_icon.png");
+}
+
+function onimageli()
+{this.setAttribute("src","img/4747495_linked_in_social_media_networking_icon.png");
+}
+
+function outimageli()
+{this.setAttribute("src","img/8010469_linkedin_job_office_business_social_media_icon.png");
+}
+
+  
